9a52afa634e456ffc8670a10ace0d188b6a37d6c
manu3210 <manu_1019@hotmail.com>
2022-02-07 10:40:10 -0300

    Ex2 Posts done!

---
+++ IntermediatePractice/EX2-Post/Program.cs
@@ -0,0 +1,53 @@
+﻿using EX2_Post;
+
+/*Design a class called Post. This class models a StackOverflow post. It should have properties
+for title, description and the date/time it was created. We should be able to up-vote or down-vote
+a post. We should also be able to see the current vote value. In the main method, create a post,
+up-vote and down-vote it a few times and then display the the current vote value.
+In this exercise, you will learn that a StackOverflow post should provide methods for up-voting
+and down-voting. You should not give the ability to set the Vote property from the outside,
+because otherwise, you may accidentally change the votes of a class to 0 or to a random
+number. And this is how we create bugs in our programs. The class should always protect its
+state and hide its implementation detail.*/
+
+static void EX2Post()
+{
+    var post = new Post();
+    Console.WriteLine("Creating a new Post!");
+    Console.WriteLine("Set a Tittle: ");
+    var tittle = Console.ReadLine();
+
+    Console.WriteLine("Set a description: ");
+    var description = Console.ReadLine();
+    
+
+    if (!string.IsNullOrWhiteSpace(tittle) && !string.IsNullOrWhiteSpace(description))
+    {
+        post.Tittle = tittle;
+        post.Description = description;
+    }
+    else
+    {
+        Console.WriteLine("Something went wrong");
+    }
+
+    Console.WriteLine("\n------------------------------------------------------------------\n");
+    Console.WriteLine("Your post: ");
+    Console.WriteLine(post.ToString());
+
+    Console.WriteLine("\n Do you like this post? Y/N");
+    var vote = Console.ReadLine();
+
+    if (vote == "y" || vote == "Y")
+    {
+        post.UpVote();
+    }
+    else
+    {
+        post.DownVote();
+    }
+
+    Console.WriteLine("\n------------------------------------------------------------------\n");
+    Console.WriteLine(post.ToString());
+}
+
--- IntermediatePractice/IntermediatePractice/Program.cs
+++ IntermediatePractice/IntermediatePractice/Program.cs
@@ -7,30 +7,35 @@ start and stop it again. Make sure the duration value each time is calculated pr
 We should not be able to start a stopwatch twice in a row (because that may overwrite the initial
 start time). So the class should throw an InvalidOperationException if its started twice. */
 
-var option = "s";
-var stopwatch = new Stopwatch();
+Ex1Stopwatch();
 
-while (option == "s")
+static void Ex1Stopwatch()
 {
-    Console.WriteLine("please, press a button to start the stopwatch");
-    Console.ReadKey();
-    stopwatch.Start();
-    Console.WriteLine("To stop the stopwatch, press a button...");
-    Console.ReadKey();
-    stopwatch.Stop();
+    var option = "y";
+    var stopwatch = new Stopwatch();
 
-    Console.WriteLine("Continue? S/N:");
-    option = Console.ReadLine();
+    while (option == "y")
+    {
+        Console.WriteLine("please, press a button to start the stopwatch");
+        Console.ReadKey();
+        stopwatch.Start();
+        Console.WriteLine("To stop the stopwatch, press a button...");
+        Console.ReadKey();
+        stopwatch.Stop();
+
+        Console.WriteLine("Continue? Y/N:");
+        option = Console.ReadLine();
+    }
+
+    Console.WriteLine("\n------------------------------------------------------------");
+    Console.WriteLine("Your time: " + stopwatch.GetTotalTime());
+    Console.WriteLine("\n");
+    Console.WriteLine("Your intervals: \n");
+
+    foreach (var item in stopwatch.Intervals)
+    {
+        Console.WriteLine(item);
+    }
+
+    Console.WriteLine("\n------------------------------------------------------------");
 }
-
-Console.WriteLine("\n------------------------------------------------------------");
-Console.WriteLine("Your time: " + stopwatch.GetTotalTime());
-Console.WriteLine("\n");
-Console.WriteLine("Your intervals: \n");
-
-foreach (var item in stopwatch.Intervals)
-{
-    Console.WriteLine(item);
-}
-
-Console.WriteLine("\n------------------------------------------------------------");
