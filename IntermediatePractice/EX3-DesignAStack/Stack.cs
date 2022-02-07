13788eada4c1df3ceb9d2758066b216b02bfaeae
manu3210 <manu_1019@hotmail.com>
2022-02-07 11:22:00 -0300

    EX3 design a stack done!

---
+++ IntermediatePractice/EX3-DesignAStack/Stack.cs
@@ -0,0 +1,39 @@
+﻿namespace EX3_DesignAStack
+{
+    public class OwnStack
+    {
+        public List<object> StackList { get; set; }
+
+        public OwnStack()
+        {
+            StackList = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+                StackList.Add(obj);
+            else
+                throw new InvalidOperationException("Null object");
+        }
+
+        public object Pop()
+        {
+            if (StackList.Count != 0)
+            {
+                var obj = StackList[StackList.Count - 1];
+                StackList.RemoveAt(StackList.Count - 1);
+                return obj;
+            }
+            else
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+        }
+
+        public void Clear()
+        {
+            StackList.Clear();
+        }
+    }
+}
